c2027451ee5801b0f46203ffe946efb82958c7d8
Sebastian Diaz <lksebas@gmail.com>
2026-06-29 19:01:53 -0300

    ...

--- AxxonProductGroups.Functions/Functions/ProductGroupSyncFunction.cs
+++ AxxonProductGroups.Functions/Functions/ProductGroupSyncFunction.cs
@@ -35,7 +35,7 @@ namespace AxxonProductGroups.Functions.Functions
 
         [Function(nameof(ProductGroupSyncFunction))]
         public async Task Run(
-            [TimerTrigger("0 */5 * * * *", RunOnStartup = true)] TimerInfo timer,
+            [TimerTrigger("%ExecutionInterval%", RunOnStartup = true)] TimerInfo timer,
             CancellationToken cancellationToken)
         {
             if (timer.IsPastDue)
